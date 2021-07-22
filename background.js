782fb6cf0a004b3fa3aae39007d5f7f4ca21c27f
Jimmy Lam <JimmyLam978@gmail.com>
2021-07-21 21:21:25 -0400

    finish setting up

--- background.js
+++ background.js
@@ -1,6 +1,27 @@
-let color = "#3aa757";
-
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({ color });
-	console.log("Default background color set to %cgreen", `color: ${color}`);
+	chrome.storage.sync.set({ editState: false });
 });
+
+let editState;
+
+chrome.storage.sync.get(['editState'], function(result) {
+	editState = result.key;
+});
+
+
+chrome.commands.onCommand.addListener((command) => {
+	editState = !editState
+	chrome.storage.sync.set({ editState });
+	const payload = editState ? "true":"false"
+	sendCommandToActiveTab(command, payload)
+});
+
+const sendCommandToActiveTab = (command, payload) => {
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		chrome.tabs.sendMessage(
+			tabs[0].id, 
+			{command, payload}, 
+			(response) => null
+		)
+	});
+}
